530825c14838cdd244428dc0f24cadff44b1142a
flazepe <51487578+flazepe@users.noreply.github.com>
2021-12-05 12:33:41 +0800

    feat: add actual statistics

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,21 +1,38 @@
+import { useEffect, useState } from "react";
+import Statistics from "types/Statistics";
 import styles from "./index.module.scss";
 
 export default function () {
+	const [{ servers, users, commands }, setStatistics] = useState({
+		servers: 0,
+		users: 0,
+		commands: 0
+	} as Statistics);
+
+	useEffect(
+		() =>
+			void fetch("https://metrics.osscord.xyz")
+				.then(_res => _res.json())
+				.then(setStatistics)
+				.catch(() => null),
+		[]
+	);
+
 	return (
 		<div id={styles.statistics}>
 			<div className={styles.statistic}>
-				<div className={styles.statisticValue}>727</div>
-				<div className={styles.statisticName}>Guilds</div>
+				<div className={styles.statisticValue}>{servers}</div>
+				<div className={styles.statisticName}>Servers</div>
 			</div>
 
 			<div className={styles.statistic}>
-				<div className={styles.statisticValue}>86</div>
+				<div className={styles.statisticValue}>{users}</div>
 				<div className={styles.statisticName}>Users</div>
 			</div>
 
 			<div className={styles.statistic}>
-				<div className={styles.statisticValue}>420</div>
-				<div className={styles.statisticName}>Another Metric</div>
+				<div className={styles.statisticValue}>{commands}</div>
+				<div className={styles.statisticName}>Commands</div>
 			</div>
 		</div>
 	);
---
+++ src/types/Statistics.ts
@@ -0,0 +1,5 @@
+export default interface Statistics {
+	servers: number;
+	users: number;
+	commands: number;
+}
