e28758f3a0c4abd033f517f205f54640cee605aa
flazepe <51487578+flazepe@users.noreply.github.com>
2021-12-12 12:28:42 +0800

    refactor: scss variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,46 @@
-import osscord from "./osscord";
+import Head from "next/head";
+import styles from "./index.module.scss";
 
-export default osscord;
+import SectionCaret from "components/SectionCaret";
+import Brand from "components/Brand";
+import Features from "components/Features";
+import Statistics from "components/Statistics";
+import Footer from "components/Footer";
+
+export default function () {
+	return (
+		<>
+			<Head>
+				<title>osscord</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0, shrink-to-fit=yes"
+				/>
+				<meta name="keywords" content="Discord bot, osu!" />
+				<meta name="theme-color" content="#ff66ab" />
+				<meta
+					name="description"
+					content="an easy-to-use, feature-rich osu! bot for discord"
+				/>
+				<meta name="og:image" content="https://osscord.vercel.app/osscord.png" />
+				<link rel="icon" type="image/png" href="/osscord.png" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<div id={styles.background} />
+
+			<Brand />
+			<SectionCaret name="Features" />
+			<Features />
+			<SectionCaret name="Statistics" />
+			<Statistics />
+
+			<Footer />
+		</>
+	);
+}
